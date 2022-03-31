85f428097581cab80956dd42b4483f1d6d4360e6
Vandi Hrustic <>
2022-03-31 17:16:10 +0200

    added all js necessary files

---
+++ facebook-clone/src/components/HEADER/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+          <img src="http://assets.stickpng.com/images/58e91965eb97430e819064f5.png" alt = "logo"/>
+      </div>
+      <div className="header__middle"></div>
+      <div className="header__right"></div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ facebook-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDkbPlaFmzkAfQhDN0TB2Pyw2ffSrqXC2s",
+    authDomain: "facebook-clone-5eae8.firebaseapp.com",
+    projectId: "facebook-clone-5eae8",
+    storageBucket: "facebook-clone-5eae8.appspot.com",
+    messagingSenderId: "782582153286",
+    appId: "1:782582153286:web:4f00fc786a70ad59d10a47"
+  };
